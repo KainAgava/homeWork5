baaec4c852296e1dcaf9d0da3f02f719f8dba7f5
KainAgava <143312166+KainAgava@users.noreply.github.com>
2023-08-28 14:42:32 +0700

    Add files via upload

---
+++ homeWork5/homeWork5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gold;
+            int crystals;
+            int crystalPrice = 10;
+
+            Console.WriteLine("Доброго времени суток, сегодня у нас акция, один кристалл стоит " + crystalPrice + " золотых");
+            
+            Console.Write("Сколько у вас с собой золота? ");
+            gold = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Сколько вы хотите приобрести кристаллов? ");
+            crystals = Convert.ToInt32(Console.ReadLine());
+
+            gold -= crystals * crystalPrice;
+            Console.WriteLine($"И того у вас {crystals} кристаллов и {gold} золотых ");
+
+        }
+
+    }
+}
